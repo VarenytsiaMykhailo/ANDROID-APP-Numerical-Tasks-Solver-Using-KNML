e39cca28954c3cd490b47e6d1a41e91e026be6eb
VarenytsiaMykhailo <52725148+VarenytsiaMykhailo@users.noreply.github.com>
2022-05-31 13:11:15 +0300

    add descriptions for methods

--- NumericalTasksSolver/app/src/main/java/com/github/varenytsiamykhailo/numericaltaskssolver/methodstypesactivities/SystemSolvingMethodsActivity.kt
+++ NumericalTasksSolver/app/src/main/java/com/github/varenytsiamykhailo/numericaltaskssolver/methodstypesactivities/SystemSolvingMethodsActivity.kt
@@ -19,24 +19,44 @@ class SystemSolvingMethodsActivity : AppCompatActivity() {
     }
 
     private fun setupViews() {
+        binding.gaussSimpleMethodInfoButton.setOnClickListener {
+            val intent = Intent(this, MethodInfoActivity::class.java)
+            intent.putExtra("methodName", "gaussSimpleMethod")
+            startActivity(intent)
+        }
         binding.gaussSimpleMethodButton.setOnClickListener {
             val intent = Intent(this, InputSystemDataActivity::class.java)
             intent.putExtra("methodName", "gaussSimpleMethod")
             startActivity(intent)
         }
 
+        binding.thomasMethodInfoButton.setOnClickListener {
+            val intent = Intent(this, MethodInfoActivity::class.java)
+            intent.putExtra("methodName", "thomasMethod")
+            startActivity(intent)
+        }
         binding.thomasMethodButton.setOnClickListener {
             val intent = Intent(this, InputSystemDataActivity::class.java)
             intent.putExtra("methodName", "thomasMethod")
             startActivity(intent)
         }
 
+        binding.jacobiMethodInfoButton.setOnClickListener {
+            val intent = Intent(this, MethodInfoActivity::class.java)
+            intent.putExtra("methodName", "jacobiMethod")
+            startActivity(intent)
+        }
         binding.jacobiMethodButton.setOnClickListener {
             val intent = Intent(this, InputSystemDataActivity::class.java)
             intent.putExtra("methodName", "jacobiMethod")
             startActivity(intent)
         }
 
+        binding.seidelMethodInfoButton.setOnClickListener {
+            val intent = Intent(this, MethodInfoActivity::class.java)
+            intent.putExtra("methodName", "seidelMethod")
+            startActivity(intent)
+        }
         binding.seidelMethodButton.setOnClickListener {
             val intent = Intent(this, InputSystemDataActivity::class.java)
             intent.putExtra("methodName", "seidelMethod")
